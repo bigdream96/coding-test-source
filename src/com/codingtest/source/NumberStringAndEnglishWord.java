e4ce44ef4d86d310e5f9d430aecd58490a9410d1
bigdream96 <bigdream9609@gmail.com>
2022-04-01 14:59:53 +0900

    feat: number string and english word question

---
+++ src/com/codingtest/source/NumberStringAndEnglishWord.java
@@ -0,0 +1,27 @@
+/**
+ * question source: https://programmers.co.kr/learn/courses/30/lessons/81301
+ */
+
+package com.codingtest.source;
+
+import java.util.Map;
+
+public class NumberStringAndEnglishWord {
+    class Solution {
+        private final Map<String, String> wordForNumStr = Map.of("zero","0","one","1","two","2","three","3","four","4","five","5","six","6","seven","7","eight","8","nine","9");
+
+        public int solution(String s) {
+            for(String word : wordForNumStr.keySet())
+                s = s.replace(word, wordForNumStr.get(word));
+
+            return Integer.parseInt(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberStringAndEnglishWord numberStringAndEnglishWord = new NumberStringAndEnglishWord();
+        Solution solution = numberStringAndEnglishWord.new Solution();
+        int result = solution.solution("one4seveneight");
+        System.out.println("result : " + result);
+    }
+}
