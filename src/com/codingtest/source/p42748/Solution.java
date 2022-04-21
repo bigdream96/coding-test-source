b178e674e5bffd028c7ff321515a97e9f50bab8a
bigdream96 <bigdream9609@gmail.com>
2022-04-21 13:53:35 +0900

    feat: the kth number

---
+++ src/com/codingtest/source/p42748/Solution.java
@@ -0,0 +1,27 @@
+package com.codingtest.source.p42748;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        int idx = 0;
+        for(int[] command : commands) {
+            int i = command[0];
+            int j = command[1];
+            int k = command[2];
+            int[] newArray = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(newArray);
+            answer[idx++] = newArray[k-1];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result = solution.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3},{4, 4, 1},{1, 7, 3}});
+        System.out.println(Arrays.toString(result));
+    }
+}
