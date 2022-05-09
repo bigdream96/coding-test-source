9fda2213c09ffb6d322cbc370ed3c9989828a80c
bigdream96 <bigdream9609@gmail.com>
2022-05-09 16:17:26 +0900

    feat: convert string to integer

---
+++ src/com/codingtest/source/programmers/p12925/Solution.java
@@ -0,0 +1,13 @@
+package com.codingtest.source.programmers.p12925;
+
+class Solution {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution("1234");
+        System.out.println(result);
+    }
+}
