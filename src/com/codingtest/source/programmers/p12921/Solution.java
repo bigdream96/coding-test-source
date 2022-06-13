8387d757dd11e1dd995943816c4096303bc415c8
youngjoon kim <bigdream9609@gmail.com>
2022-06-13 14:23:30 +0900

    refactor: sieve of eratosthenes

--- src/com/codingtest/source/programmers/p12921/Solution.java
+++ src/com/codingtest/source/programmers/p12921/Solution.java
@@ -2,26 +2,26 @@ package com.codingtest.source.programmers.p12921;
 
 class Solution {
     public int solution(int n) {
+        int result = 0;
         boolean[] arr = new boolean[n + 1];
 
-        for(int i = 2; i <= n; i++) {
+        for(int i = 2; i <= Math.sqrt(n); i++) {
             if(arr[i])
                 continue;
             for(int j = 2 * i; j <= n; j += i)
                 arr[j] = true;
         }
 
-        int count = 0;
         for(int i = 2; i <= n; i++)
             if(!arr[i])
-                count++;
+                result++;
 
-        return count;
+        return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int result = solution.solution(1000);
+        int result = solution.solution(100);
         System.out.println(result);
     }
 }
