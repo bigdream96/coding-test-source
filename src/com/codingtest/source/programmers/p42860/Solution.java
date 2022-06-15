392be0895ef7e2194f26fa9daeecf8553366f6b3
youngjoon kim <bigdream9609@gmail.com>
2022-06-15 20:45:29 +0900

    feat: joystick

---
+++ src/com/codingtest/source/programmers/p42860/Solution.java
@@ -0,0 +1,28 @@
+package com.codingtest.source.programmers.p42860;
+
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int move = name.length() - 1;
+
+        int idx;
+        for (int i = 0; i < name.length(); i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            idx = i + 1;
+
+            while (idx < name.length() && name.charAt(idx) == 'A')
+                idx++;
+
+            move = Math.min(move, (i * 2 + name.length()) - idx);
+            move = Math.min(move, (name.length() - idx) * 2 + i);
+        }
+
+        return answer + move;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution("JAN");
+        System.out.println(result);
+    }
+}
