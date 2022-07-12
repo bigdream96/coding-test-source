7cbe61f9d76b0f3bbf3a722b4ddc790fc625468c
youngjoon kim <bigdream9609@gmail.com>
2022-07-12 17:43:59 +0900

    feat: add test case

--- src/test/java/com/codingtest/source/programmers/p92334/SolutionTest.java
+++ src/test/java/com/codingtest/source/programmers/p92334/SolutionTest.java
@@ -15,11 +15,25 @@ class SolutionTest {
     }
 
     @Test
-    @DisplayName("CASE1. 한 유저가 같은 유저를 신고한 경우에는 신고 횟수 1회")
-    void solution() {
+    @DisplayName("CASE1. 문제예시")
+    void testCase1() {
+        String[] idList = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
+        int[] result = solution.solution(idList, report, k);
+
+        assertEquals(result[0], 2);
+        assertEquals(result[1], 1);
+        assertEquals(result[2], 1);
+        assertEquals(result[3], 0);
+    }
+
+    @Test
+    @DisplayName("CASE2. 한 유저가 같은 유저를 신고한 경우에는 신고 횟수 1회")
+    void testCase2() {
         String[] idList = {"con", "ryan"};
         String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
-        int k = 2;
+        int k = 3;
         int[] result = solution.solution(idList, report, k);
 
         assertEquals(result[0], 0);
