d6f628cdd71d94a8651b56867d45f110788b7cd1
youngjoon kim <bigdream9609@gmail.com>
2022-07-12 15:29:19 +0900

    refactor: code cleanup

--- src/com/codingtest/source/programmers/p92334/Solution.java
+++ src/com/codingtest/source/programmers/p92334/Solution.java
@@ -1,51 +1,47 @@
-/**
- * question source: https://programmers.co.kr/learn/courses/30/lessons/92334
- */
-
 package com.codingtest.source.programmers.p92334;
 
 import java.util.*;
 
 class Solution {
-    final String SEPARATOR = " ";
-    Map<String, List<String>> reportListMap = new LinkedHashMap<>();
-    Map<String, Integer> cntForReport = new LinkedHashMap<>();
-    Map<String, Integer> cntForMail = new LinkedHashMap<>();
-
-    public int[] solution(String[] idList, String[] reportList, int k) {
-        for(String id : idList) {
-            reportListMap.put(id, new LinkedList<>());
-            cntForReport.put(id, 0);
-            cntForMail.put(id, 0);
-        }
-
-        for(String report : reportList) {
-            String[] reportData = report.split(SEPARATOR);
-            String id = reportData[0];
-            String reportId = reportData[1];
-            if(!reportListMap.get(id).contains(reportId))
-                reportListMap.get(id).add(reportId);
-        }
-
-        for(List<String> reportIdList : reportListMap.values())
-            for(String reportId : reportIdList)
-                cntForReport.put(reportId, cntForReport.get(reportId) + 1);
-
-        for(String id : reportListMap.keySet())
-            for(String reportId : reportListMap.get(id))
-                if(cntForReport.get(reportId) >= k)
-                    cntForMail.put(id, cntForMail.get(id) + 1);
-
-        return cntForMail.values().stream().mapToInt(i->i).toArray();
-    }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         String[] idList = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
         int k = 2;
-
         int[] result = solution.solution(idList, report, k);
         System.out.println(Arrays.toString(result));
+    }
+
+    public int[] solution(String[] idList, String[] reportList, int k) {
+        int[] answer = new int[idList.length];
+        Map<String, Set<String>> reportListById = new LinkedHashMap<>();
+        Map<String, Integer> cntByReportId = new LinkedHashMap<>();
+        Map<String, Integer> cntByMailId = new LinkedHashMap<>();
+
+        for (String id : idList) {
+            reportListById.put(id, new LinkedHashSet<>());
+            cntByReportId.put(id, 0);
+            cntByMailId.put(id, 0);
+        }
+
+        for (String report : reportList) {
+            String[] data = report.split(" ");
+            reportListById.get(data[0]).add(data[1]);
+        }
+
+        for (String id : reportListById.keySet())
+            for (String reportId : reportListById.get(id))
+                cntByReportId.put(reportId, cntByReportId.get(reportId) + 1);
+
+        for (String id : reportListById.keySet())
+            for (String reportId : reportListById.get(id))
+                if (cntByReportId.get(reportId) >= k)
+                    cntByMailId.put(id, cntByMailId.get(id) + 1);
+
+        int i = 0;
+        for (String id : cntByMailId.keySet())
+            answer[i++] = cntByMailId.get(id);
+
+        return answer;
     }
 }
