04260551ba5831bc2d305135ac5bb5108e27953a
youngjoon kim <bigdream9609@gmail.com>
2022-07-13 19:19:34 +0900

    refactor: string compression
    - code cleanup
    - rename variable, method, other
    - change data structure

--- src/main/java/com/codingtest/source/programmers/p60057/Solution.java
+++ src/main/java/com/codingtest/source/programmers/p60057/Solution.java
@@ -1,33 +1,37 @@
 package com.codingtest.source.programmers.p60057;
 
+import java.util.LinkedList;
+
 class Solution {
     public int solution(String s) {
         int answer = s.length();
 
-        for(int i = 1; i <= s.length(); i++) {
+        for (int i = 1; i <= s.length(); i++) {
             StringBuilder result = new StringBuilder(s.length());
-            String[] items = s.split("(?<=\\G[a-z]{" + i + "})");
-            String curText = items[0];
+            LinkedList<String> items = new LinkedList<>();
+
+            int idx = 0;
+            while (idx < s.length()) {
+                items.add(s.substring(idx, Math.min(idx + i, s.length())));
+                idx += i;
+            }
+
+            String curItem = items.removeFirst();
             int count = 1;
-            for(int j = 1; j < items.length; j++) {
-                if(items[j].equals(curText)) {
+            for (String item : items) {
+                if (item.equals(curItem)) {
                     count++;
                 } else {
-                    result.append(count > 1 ? count : "").append(curText);
-                    curText = items[j];
+                    result.append(count > 1 ? count : "").append(curItem);
+                    curItem = item;
                     count = 1;
                 }
             }
-            result.append(count > 1 ? count : "").append(curText);
+
+            result.append(count > 1 ? count : "").append(curItem);
             answer = Math.min(result.length(), answer);
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int result = solution.solution("xababcdcdababcdcd");
-        System.out.println(result);
     }
 }
