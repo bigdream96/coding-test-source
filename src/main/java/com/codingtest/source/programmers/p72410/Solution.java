61b5a1b6612720bb88cfb67371fa184afdba5688
youngjoon kim <bigdream9609@gmail.com>
2022-07-13 13:43:33 +0900

    refactor: new id recommendation question
    - code clean
    - rename variable, method

--- src/main/java/com/codingtest/source/programmers/p72410/Solution.java
+++ src/main/java/com/codingtest/source/programmers/p72410/Solution.java
@@ -2,31 +2,23 @@ package com.codingtest.source.programmers.p72410;
 
 class Solution {
     public String solution(String newId) {
-        return validate(newId) ? newId : generateNewId(newId);
-    }
-
-    private boolean validate(String id) {
-        return id.matches("^[a-z0-9\\_\\-][a-z0-9\\_\\-\\.]{1,13}[a-z0-9\\_\\-]$") &&
-                id.matches("^[^\\.]*[\\.]?[^\\.]*$");
-    }
-
-    private String generateNewId(String id) {
-        StringBuilder result = new StringBuilder(id.toLowerCase()
+        StringBuilder sb = new StringBuilder(newId.toLowerCase()
                 .replaceAll("[^a-z0-9\\_\\.\\-]", "")
                 .replaceAll("[\\.]{2,}", "\\.")
                 .replaceAll("^(\\.)|(\\.)$", ""));
 
-        if (result.toString().equals(""))
-            result = new StringBuilder("a");
+        if (sb.length() == 0)
+            sb.append("a");
 
-        if (result.length() >= 16) {
-            String tmp = result.substring(0, 15);
-            result = new StringBuilder(tmp.endsWith(".") ? tmp.substring(0, 14) : tmp);
+        if (sb.length() >= 16) {
+            sb.delete(15, sb.length());
+            if (sb.charAt(sb.length() - 1) == '.')
+                sb.deleteCharAt(sb.length() - 1);
         }
 
-        while (result.length() <= 2)
-            result.append(result.charAt(result.length() - 1));
+        while (sb.length() <= 2)
+            sb.append(sb.charAt(sb.length() - 1));
 
-        return result.toString();
+        return sb.toString();
     }
 }
