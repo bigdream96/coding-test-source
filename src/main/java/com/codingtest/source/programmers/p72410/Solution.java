44a66f1277dc018129ea1b59e6ac331c82c91666
youngjoon kim <bigdream9609@gmail.com>
2022-07-13 11:19:25 +0900

    refactor: new id recommendation question
    - code clean
    - String -> StringBuilder

--- src/main/java/com/codingtest/source/programmers/p72410/Solution.java
+++ src/main/java/com/codingtest/source/programmers/p72410/Solution.java
@@ -1,7 +1,3 @@
-/**
- * question source: https://programmers.co.kr/learn/courses/30/lessons/72410
- */
-
 package com.codingtest.source.programmers.p72410;
 
 class Solution {
@@ -15,31 +11,22 @@ class Solution {
     }
 
     private String generateNewId(String id) {
-        String result = id.toLowerCase()
+        StringBuilder result = new StringBuilder(id.toLowerCase()
                 .replaceAll("[^a-z0-9\\_\\.\\-]", "")
                 .replaceAll("[\\.]{2,}", "\\.")
-                .replaceAll("^(\\.)|(\\.)$", "");
+                .replaceAll("^(\\.)|(\\.)$", ""));
 
-        if(result.equals(""))
-            result = "a";
+        if (result.toString().equals(""))
+            result = new StringBuilder("a");
 
-        if(result.length() >= 16) {
+        if (result.length() >= 16) {
             String tmp = result.substring(0, 15);
-            result = tmp.endsWith(".") ? tmp.substring(0, 14) : tmp;
+            result = new StringBuilder(tmp.endsWith(".") ? tmp.substring(0, 14) : tmp);
         }
 
-        while(result.length() <= 2) {
-            result += result.charAt(result.length()-1);
-            if(result.length() == 3)
-                break;
-        }
+        while (result.length() <= 2)
+            result.append(result.charAt(result.length() - 1));
 
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String result = solution.solution("=.=");
-        System.out.println(result);
+        return result.toString();
     }
 }
