72bb3139598748c8577d0c8bf2997584367b87e2
youngjoon kim <bigdream9609@gmail.com>
2022-07-14 14:43:18 +0900

    refactor: open chat room
    - code cleanup
    - improve code readability

--- src/main/java/com/codingtest/source/programmers/p42888/Solution.java
+++ src/main/java/com/codingtest/source/programmers/p42888/Solution.java
@@ -8,29 +8,26 @@ class Solution {
         Map<String, String> nameById = new HashMap<>();
         Map<String, String> messageByCommand = Map.of("Enter", "님이 들어왔습니다.", "Leave", "님이 나갔습니다.");
 
-        for(String record : records) {
+        for (String record : records) {
             String[] arr = record.split(" ");
             String command = arr[0];
             String id = arr[1];
 
-            if(command.equals("Enter") || command.equals("Change"))
-                nameById.put(id, arr[2]);
+            if (command.equals("Enter") || command.equals("Change")) {
+                String nickname = arr[2];
+                nameById.put(id, nickname);
+            }
 
-            if(!command.equals("Change"))
-                messageItems.add(List.of(messageByCommand.get(command), id));
+            if (!command.equals("Change")) {
+                messageItems.add(List.of(id, command));
+            }
         }
 
         String[] result = new String[messageItems.size()];
         int i = 0;
-        for(List<String> messageItem : messageItems)
-            result[i++] = nameById.get(messageItem.get(1)) + messageItem.get(0);
+        for (List<String> messageItem : messageItems)
+            result[i++] = nameById.get(messageItem.get(0)) + messageByCommand.get(messageItem.get(1));
 
         return result;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String[] result = solution.solution(new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"});
-        System.out.println(Arrays.toString(result));
     }
 }
