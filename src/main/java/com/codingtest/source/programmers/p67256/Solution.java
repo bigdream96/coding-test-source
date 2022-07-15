315226cfd78159a132750200507246ce60f731b7
youngjoon kim <bigdream9609@gmail.com>
2022-07-15 13:16:04 +0900

    refactor: keypad press
    - code cleanup

--- src/main/java/com/codingtest/source/programmers/p67256/Solution.java
+++ src/main/java/com/codingtest/source/programmers/p67256/Solution.java
@@ -1,34 +1,42 @@
 package com.codingtest.source.programmers.p67256;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    StringBuilder result = new StringBuilder();
-    HashMap<String, String> handToNumber = new HashMap<>(Map.of("left", "*", "right", "#"));
-    HashMap<String, String> handToPrintText = new HashMap<>(Map.of("left", "L", "right", "R"));
-    HashMap<String, List<Integer>> numberToPosition = new HashMap<>();
-    String[] type = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"};
-
     public String solution(int[] numbers, String hand) {
-        init(numberToPosition);
+        StringBuilder result = new StringBuilder();
+        Map<String, String> handToNumber = new HashMap<>(Map.of("left", "*", "right", "#"));
+        Map<String, String> handToPrintText = new HashMap<>(Map.of("left", "L", "right", "R"));
+        Map<String, List<Integer>> numberToPosition = new HashMap<>();
+        List<String> types = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#");
 
-        for(int number : numbers) {
+        int idx = 0;
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 3; j++)
+                numberToPosition.put(types.get(idx++), List.of(i, j));
+
+        for (int number : numbers) {
             String key = String.valueOf(number);
             String moveNumber = "";
             switch (number) {
-                case 1: case 4: case 7:
+                case 1:
+                case 4:
+                case 7:
                     moveNumber = "left";
                     break;
-                case 3: case 6: case 9:
+                case 3:
+                case 6:
+                case 9:
                     moveNumber = "right";
                     break;
-                case 2: case 5: case 8: case 0:
+                case 2:
+                case 5:
+                case 8:
+                case 0:
                     int left = getDistance(numberToPosition, handToNumber, key, "left");
                     int right = getDistance(numberToPosition, handToNumber, key, "right");
-                    if(left < right) moveNumber = "left";
-                    else if(left > right) moveNumber = "right";
+                    if (left < right) moveNumber = "left";
+                    else if (left > right) moveNumber = "right";
                     else moveNumber = hand;
                     break;
             }
@@ -39,21 +47,8 @@ class Solution {
         return result.toString();
     }
 
-    void init(HashMap<String, List<Integer>> numberToPosition) {
-        int typeIdx = 0;
-        for(int i=0; i<4; i++)
-            for(int j=0; j<3; j++)
-                numberToPosition.put(type[typeIdx++], List.of(i, j));
-    }
-
-    int getDistance(HashMap<String, List<Integer>> numberToPosition, HashMap<String, String> handToNumber, String number, String hand) {
+    int getDistance(Map<String, List<Integer>> numberToPosition, Map<String, String> handToNumber, String number, String hand) {
         return Math.abs(numberToPosition.get(number).get(0) - numberToPosition.get(handToNumber.get(hand)).get(0))
                 + Math.abs(numberToPosition.get(number).get(1) - numberToPosition.get(handToNumber.get(hand)).get(1));
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String result = solution.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, "right");
-        System.out.println(result);
     }
 }
