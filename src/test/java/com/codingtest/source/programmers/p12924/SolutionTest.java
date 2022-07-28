92cc16f81e80461822dad6abd5c8763dcf577228
youngjoon kim <bigdream9609@gmail.com>
2022-07-28 16:38:34 +0900

    test: representation of numbers

---
+++ src/test/java/com/codingtest/source/programmers/p12924/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.codingtest.source.programmers.p12924;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("숫자의 표현")
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    @DisplayName("문제 예시1")
+    void testCase1() {
+        int n = 15;
+
+        int answer = this.solution.solution(n);
+
+        assertEquals(4, answer);
+    }
+
+    @Test
+    @DisplayName("테스트값1 - 제한사항의 최대값")
+    void testCase2() {
+        int n = 10000;
+
+        int answer = this.solution.solution(n);
+
+        assertEquals(5, answer);
+    }
+}
