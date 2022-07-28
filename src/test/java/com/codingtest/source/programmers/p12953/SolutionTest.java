259d66ef0fedd0cbc08101b73304d3147998d5fe
youngjoon kim <bigdream9609@gmail.com>
2022-07-28 12:39:09 +0900

    test: n least common multiples

---
+++ src/test/java/com/codingtest/source/programmers/p12953/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.codingtest.source.programmers.p12953;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("N개의 최소공배수")
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    @DisplayName("문제 예시1")
+    void testCase1() {
+        int[] arr = new int[]{2,6,8,14};
+
+        int answer = solution.solution(arr);
+
+        assertEquals(168, answer);
+    }
+
+    @Test
+    @DisplayName("문제 예시2")
+    void testCase2() {
+        int[] arr = new int[]{1,2,3};
+
+        int answer = solution.solution(arr);
+
+        assertEquals(6, answer);
+    }
+}
