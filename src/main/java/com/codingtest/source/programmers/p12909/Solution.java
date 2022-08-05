522052e468e64d177aa690d9514e3f91a6b6543a
youngjoon <bigdream9609@gmail.com>
2022-08-05 16:44:07 +0900

    feat: correct parenthesis

---
+++ src/main/java/com/codingtest/source/programmers/p12909/Solution.java
@@ -0,0 +1,22 @@
+package com.codingtest.source.programmers.p12909;
+
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
